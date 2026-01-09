88b96755c3ed7f59115597c48d9a2cdc7a33b7d0
Rohan Puri <purirohan62@gmail.com>
2026-01-09 14:18:51 -0500

    feat: add constraint to Leave - StartDate must be on or before EndDate
    - validation for employees requesting time off

--- Models/Leave.cs
+++ Models/Leave.cs
@@ -1,10 +1,24 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace TeamManager.Models;
 
-public class Leave
+
+public class Leave : IValidatableObject
 {
     public int Id { get; set; }
     public string UserId { get; set; } = null!;
-    public string StartDate { get; set; } = null!;
-    public string EndDate { get; set; } = null!;
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
     public string Status { get; set; } = "Pending";
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (StartDate > EndDate)
+        {
+            yield return new ValidationResult(
+                "The start date must be on or before the end date.",
+                [nameof(StartDate), nameof(EndDate)]);
+        }
+    }
 }
