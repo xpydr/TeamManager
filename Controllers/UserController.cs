43416582f81a35e722b14206c7e796a3c6eb2794
Rohan Puri <purirohan62@gmail.com>
2026-01-10 11:45:28 -0500

    refactor: move business logic from UserController to UserService

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,44 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using TeamManager.Models;
 using TeamManager.Services;
-using TeamManager.Mappings;
-using TeamManager.Data;
 
 namespace TeamManager.Controllers;
 
 [ApiController]
 [Route("api/users")]
-public class UserController : ControllerBase
+public class UserController(UserService userService) : ControllerBase
 {
-    private readonly UserService _userService;
-    private readonly AppDbContext _context;
-
-    public UserController(UserService userService, AppDbContext context)
-    {
-        _userService = userService;
-        _context = context;
-    }
-
     [HttpGet("{id}")]
     public async Task<ActionResult<UserDto>> GetUser(int id)
     {
-        var user = await _context.Users.FindAsync(id);
-        if (user == null) return NotFound();
-
-        return Ok(user.ToDto());
+        var result = await userService.GetUserByIdAsync(id);
+        return result is null ? NotFound() : Ok(result);
     }
     [HttpGet]
     public async Task<IActionResult> GetUsers()
     {
-        var users = await _userService.GetAllUsersAsync();
+        var users = await userService.GetAllUsersAsync();
         return Ok(users);
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateUser(User user)
     {
-        var created = await _userService.CreateUserAsync(user);
+        var created = await userService.CreateUserAsync(user);
         return Ok(created);
     }
-
 }
