93e003ac16070b5ea9b0a780c2aa30f35be272e6
Rohan Puri <purirohan62@gmail.com>
2026-01-09 19:11:23 -0500

    feat: add UserDto to UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using TeamManager.Models;
 using TeamManager.Services;
+using TeamManager.Mappings;
+using TeamManager.Data;
 
 namespace TeamManager.Controllers;
 
@@ -9,12 +11,22 @@ namespace TeamManager.Controllers;
 public class UserController : ControllerBase
 {
     private readonly UserService _userService;
+    private readonly AppDbContext _context;
 
-    public UserController(UserService userService)
+    public UserController(UserService userService, AppDbContext context)
     {
         _userService = userService;
+        _context = context;
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> GetUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null) return NotFound();
+
+        return Ok(user.ToDto());
+    }
     [HttpGet]
     public async Task<IActionResult> GetUsers()
     {
@@ -28,4 +40,5 @@ public class UserController : ControllerBase
         var created = await _userService.CreateUserAsync(user);
         return Ok(created);
     }
+
 }
---
+++ Dtos/UserDto.cs
@@ -0,0 +1,6 @@
+public class UserDto
+{
+    public int Id { get; init; }
+    public string Email { get; init; } = null!;
+    public string Role { get; init; } = null!;
+}
